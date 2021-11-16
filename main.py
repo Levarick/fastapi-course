cef79c308365c30675c4ac10f98925b9bb33a948
Ricardo León Valencia <rvalencia765@gmail.com>
2021-11-15 18:04:00 -0600

    request abd responsive

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-from typing import Dict
-from fastapi import FastAPI
+#Python
+from typing import Dict, Optional
+
+#Pydantic
+from pydantic import BaseModel
+
+#FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
+
+#Models
+
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
 
 @app.get("/")
 def home() -> Dict:
     return {"Hello": "World"}
+
+# Request and Response
+
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person
